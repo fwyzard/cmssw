8b2c21163b7d9e3db148b5c823b93b5056231edc
Marino Missiroli <m.missiroli@cern.ch>
2021-02-13 22:39:16 +0100

    further cleanup of HLT_75e33.py

--- HLTrigger/Phase2/python/HLT_75e33/modules/pfPileUpJME_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pfPileUpJME = cms.EDProducer("PFPileUp",
-    Enable = cms.bool(True),
-    PFCandidates = cms.InputTag("particleFlowPtrs"),
-    Vertices = cms.InputTag("goodOfflinePrimaryVertices"),
-    checkClosestZVertex = cms.bool(False),
-    verbose = cms.untracked.bool(False)
-)
