a86b2457cae39d389d2acc28573b65e111c1505a
Andrea Bocci <andrea.bocci@cern.ch>
2022-05-18 14:51:01 +0200

    Update the test configurations
    Update the writer test configuration to use a SwitchProducerCUDA.
    Update the names of the modules to not have Alpaka in the name.

--- HeterogeneousCore/AlpakaTest/test/reader.py
+++ HeterogeneousCore/AlpakaTest/test/reader.py
@@ -1,26 +1,26 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("Reader")
+process = cms.Process('Reader')
 
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:xyzid.root')
+process.source = cms.Source('PoolSource',
+    fileNames = cms.untracked.vstring('file:test.root')
 )
 
-process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('FWCore.MessageService.MessageLogger_cfi')
 
-process.AlpakaServiceSerialSync = cms.Service("AlpakaServiceSerialSync")
+process.AlpakaServiceSerialSync = cms.Service('AlpakaServiceSerialSync')
 process.MessageLogger.AlpakaService = cms.untracked.PSet()
 
-process.xyzIdAlpakaAnalyzerFromCuda = cms.EDAnalyzer('XyzIdAlpakaAnalyzer',
-    source = cms.InputTag('xyzIdAlpakaTranscriberFromCuda')
+process.testAnalyzer = cms.EDAnalyzer('XyzIdAlpakaAnalyzer',
+    source = cms.InputTag('testProducer')
 )
 
-process.xyzIdAlpakaAnalyzerSerial = cms.EDAnalyzer('XyzIdAlpakaAnalyzer',
-    source = cms.InputTag('xyzIdAlpakaProducerSerial')
+process.testAnalyzerSerial = cms.EDAnalyzer('XyzIdAlpakaAnalyzer',
+    source = cms.InputTag('testProducerSerial')
 )
 
-process.cuda_path = cms.Path(process.xyzIdAlpakaAnalyzerFromCuda)
+process.cuda_path = cms.Path(process.testAnalyzer)
 
-process.serial_path = cms.Path(process.xyzIdAlpakaAnalyzerSerial)
+process.serial_path = cms.Path(process.testAnalyzerSerial)
 
 process.maxEvents.input = 10
--- HeterogeneousCore/AlpakaTest/test/writer.py
+++ HeterogeneousCore/AlpakaTest/test/writer.py
@@ -1,55 +1,69 @@
 import FWCore.ParameterSet.Config as cms
+from HeterogeneousCore.CUDACore.SwitchProducerCUDA import SwitchProducerCUDA
 
-process = cms.Process("Writer")
+process = cms.Process('Writer')
 
-process.source = cms.Source("EmptySource")
+process.source = cms.Source('EmptySource')
 
-process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.StandardSequences.Accelerators_cff')
 
-process.add_(cms.Service("CUDAService"))
-process.MessageLogger.CUDAService = cms.untracked.PSet()
-
-process.AlpakaServiceCudaAsync = cms.Service("AlpakaServiceCudaAsync")
-process.AlpakaServiceSerialSync = cms.Service("AlpakaServiceSerialSync")
+process.AlpakaServiceCudaAsync = cms.Service('AlpakaServiceCudaAsync')
+process.AlpakaServiceSerialSync = cms.Service('AlpakaServiceSerialSync')
 process.MessageLogger.AlpakaService = cms.untracked.PSet()
 
-process.xyzIdAlpakaProducerCuda = cms.EDProducer('alpaka_cuda_async::XyzIdAlpakaProducer',
+process.testProducerCuda = cms.EDProducer('alpaka_cuda_async::XyzIdAlpakaProducer',
     size = cms.int32(42)
 )
 
-process.xyzIdAlpakaTranscriberFromCuda = cms.EDProducer('alpaka_cuda_async::XyzIdAlpakaTranscriber',
-    source = cms.InputTag('xyzIdAlpakaProducerCuda')
+process.testTranscriberFromCuda = cms.EDProducer('alpaka_cuda_async::XyzIdAlpakaTranscriber',
+    source = cms.InputTag('testProducerCuda')
 )
 
-process.xyzIdAlpakaAnalyzerFromCuda = cms.EDAnalyzer('XyzIdAlpakaAnalyzer',
-    source = cms.InputTag('xyzIdAlpakaTranscriberFromCuda')
-)
-
-process.xyzIdAlpakaProducerSerial = cms.EDProducer('alpaka_serial_sync::XyzIdAlpakaProducer',
+process.testProducerCpu = cms.EDProducer('alpaka_serial_sync::XyzIdAlpakaProducer',
     size = cms.int32(42)
 )
 
-process.xyzIdAlpakaAnalyzerSerial = cms.EDAnalyzer('XyzIdAlpakaAnalyzer',
-    source = cms.InputTag('xyzIdAlpakaProducerSerial')
+process.testProducer = SwitchProducerCUDA(
+    cpu = cms.EDAlias(
+        testProducerCpu = cms.VPSet(cms.PSet(type = cms.string('*')))
+    ),
+    cuda = cms.EDAlias(
+        testTranscriberFromCuda = cms.VPSet(cms.PSet(type = cms.string('*')))
+    )
 )
 
-process.output = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string("xyzid.root"),
+process.testAnalyzer = cms.EDAnalyzer('XyzIdAlpakaAnalyzer',
+    source = cms.InputTag('testProducer')
+)
+
+process.testProducerSerial = cms.EDProducer('alpaka_serial_sync::XyzIdAlpakaProducer',
+    size = cms.int32(99)
+)
+
+process.testAnalyzerSerial = cms.EDAnalyzer('XyzIdAlpakaAnalyzer',
+    source = cms.InputTag('testProducerSerial')
+)
+
+process.output = cms.OutputModule('PoolOutputModule',
+    fileName = cms.untracked.string('test.root'),
     outputCommands = cms.untracked.vstring(
         'drop *',
-        'keep *_xyzIdAlpakaTranscriberFromCuda_*_*',
-        'keep *_xyzIdAlpakaProducerSerial_*_*',
+        'keep *_testProducer_*_*',
+        'keep *_testProducerSerial_*_*',
   )
 )
 
-process.cuda_path = cms.Path(
-    process.xyzIdAlpakaProducerCuda +
-    process.xyzIdAlpakaTranscriberFromCuda +
-    process.xyzIdAlpakaAnalyzerFromCuda)
+process.producer_task = cms.Task(process.testProducerCuda, process.testTranscriberFromCuda, process.testProducerCpu)
+
+process.process_path = cms.Path(
+    process.testProducer +
+    process.testAnalyzer,
+    process.producer_task)
 
 process.serial_path = cms.Path(
-    process.xyzIdAlpakaProducerSerial +
-    process.xyzIdAlpakaAnalyzerSerial)
+    process.testProducerSerial +
+    process.testAnalyzerSerial)
 
 process.output_path = cms.EndPath(process.output)
 
