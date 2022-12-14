8758d4408cdebc86c4254852132dca5907679021
Andrea Bocci <andrea.bocci@cern.ch>
2022-12-14 08:52:44 +0100

    Rename static data members following the coding conventions

--- DataFormats/Portable/interface/PortableDeviceCollection.h
+++ DataFormats/Portable/interface/PortableDeviceCollection.h
@@ -25,10 +25,11 @@ class PortableDeviceCollection {
   static_assert(not std::is_same<T3, void>::value or std::is_same<T4, void>::value);
   static_assert(not std::is_same<T2, void>::value or std::is_same<T3, void>::value);
   static_assert(not std::is_same<T1, void>::value or std::is_same<T2, void>::value);
-  template <typename T>
-  static constexpr std::size_t typeCount = CollectionTypeCount<T, T0, T1, T2, T3, T4>;
 
-  static constexpr std::size_t membersCount = CollectionMembersCount<T0, T1, T2, T3, T4>;
+  template <typename T>
+  static constexpr std::size_t count_t_ = CollectionTypeCount<T, T0, T1, T2, T3, T4>;
+
+  static constexpr std::size_t members_ = CollectionMembersCount<T0, T1, T2, T3, T4>;
 
 public:
   using Buffer = cms::alpakatools::device_buffer<TDev, std::byte[]>;
@@ -38,13 +39,13 @@ public:
   template <typename T>
   static constexpr auto IdxResolver = CollectionIdxResolver<T, T0, T1, T2, T3, T4>;
 
-  using SizesArray = std::array<int32_t, membersCount>;
+  using SizesArray = std::array<int32_t, members_>;
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   using Layout = CollectionTypeResolver<Idx, T0, T1, T2, T3, T4>;
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   using View = typename Layout<Idx>::View;
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   using ConstView = typename Layout<Idx>::ConstView;
 
 private:
@@ -76,7 +77,7 @@ public:
         impl_{buffer_->data(), elements} {
     // Alpaka set to a default alignment of 128 bytes defining ALPAKA_DEFAULT_HOST_MEMORY_ALIGNMENT=128
     assert(reinterpret_cast<uintptr_t>(buffer_->data()) % Layout<>::alignment == 0);
-    static_assert(membersCount == 1);
+    static_assert(members_ == 1);
   }
 
   template <typename TQueue, typename = std::enable_if_t<cms::alpakatools::is_queue_v<TQueue>>>
@@ -85,12 +86,12 @@ public:
         impl_{buffer_->data(), elements} {
     // Alpaka set to a default alignment of 128 bytes defining ALPAKA_DEFAULT_HOST_MEMORY_ALIGNMENT=128
     assert(reinterpret_cast<uintptr_t>(buffer_->data()) % Layout<>::alignment == 0);
-    static_assert(membersCount == 1);
+    static_assert(members_ == 1);
   }
 
   static int32_t computeDataSize(const SizesArray& sizes) {
     int32_t ret = 0;
-    constexpr_for<0, membersCount>([&sizes, &ret](auto i) { ret += Layout<i>::computeDataSize(sizes[i]); });
+    constexpr_for<0, members_>([&sizes, &ret](auto i) { ret += Layout<i>::computeDataSize(sizes[i]); });
     return ret;
   }
 
@@ -98,10 +99,10 @@ public:
       : buffer_{cms::alpakatools::make_device_buffer<std::byte[]>(device, computeDataSize(sizes))},
         impl_{buffer_->data(), sizes} {
     // Alpaka set to a default alignment of 128 bytes defining ALPAKA_DEFAULT_HOST_MEMORY_ALIGNMENT=128
-    constexpr_for<0, membersCount>(
+    constexpr_for<0, members_>(
         [&](auto i) { assert(reinterpret_cast<uintptr_t>(buffer_->data()) % Layout<i>::alignment == 0); });
     constexpr auto alignment = Layout<0>::alignment;
-    constexpr_for<1, membersCount>([&alignment](auto i) { static_assert(alignment == Layout<i>::alignment); });
+    constexpr_for<1, members_>([&alignment](auto i) { static_assert(alignment == Layout<i>::alignment); });
   }
 
   template <typename TQueue, typename = std::enable_if_t<cms::alpakatools::is_queue_v<TQueue>>>
@@ -109,10 +110,10 @@ public:
       : buffer_{cms::alpakatools::make_device_buffer<std::byte[]>(queue, computeDataSize(sizes))},
         impl_{buffer_->data(), sizes} {
     // Alpaka set to a default alignment of 128 bytes defining ALPAKA_DEFAULT_HOST_MEMORY_ALIGNMENT=128
-    constexpr_for<0, membersCount>(
+    constexpr_for<0, members_>(
         [&](auto i) { assert(reinterpret_cast<uintptr_t>(buffer_->data()) % Layout<i>::alignment == 0); });
     constexpr auto alignment = Layout<0>::alignment;
-    constexpr_for<1, membersCount>([&alignment](auto i) { static_assert(alignment == Layout<i>::alignment); });
+    constexpr_for<1, members_>([&alignment](auto i) { static_assert(alignment == Layout<i>::alignment); });
   }
 
   // non-copyable
@@ -127,37 +128,37 @@ public:
   ~PortableDeviceCollection() = default;
 
   // access the View by index
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   View<Idx>& view() {
     return get<Idx>().view_;
   }
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   ConstView<Idx> const& view() const {
     return get<Idx>().view_;
   }
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   ConstView<Idx> const& const_view() const {
     return get<Idx>().view_;
   }
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   View<Idx>& operator*() {
     return get<Idx>().view_;
   }
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   ConstView<Idx> const& operator*() const {
     return get<Idx>().view_;
   }
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   View<Idx>* operator->() {
     return &get<Idx>().view_;
   }
 
-  template <std::size_t Idx = 0, typename = std::enable_if_t<(membersCount > Idx)>>
+  template <std::size_t Idx = 0, typename = std::enable_if_t<(members_ > Idx)>>
   ConstView<Idx> const* operator->() const {
     return &get<Idx>().view_;
   }
@@ -206,7 +207,7 @@ public:
   // Extract the sizes array
   SizesArray sizes() const {
     SizesArray ret;
-    constexpr_for<0, membersCount>([&](auto i) { ret[i] = get<i>().layout_.metadata().size(); });
+    constexpr_for<0, members_>([&](auto i) { ret[i] = get<i>().layout_.metadata().size(); });
     return ret;
   }
 
