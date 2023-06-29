cc855ee8dac70a1fdef10f567352c5a9d4ccff38
Andrea Bocci <andrea.bocci@cern.ch>
2023-06-29 07:47:00 +0200

    Update SiPixelClusterSoA dictionaries

--- DataFormats/SiPixelClusterSoA/src/alpaka/classes_cuda.h
+++ DataFormats/SiPixelClusterSoA/src/alpaka/classes_cuda.h
@@ -1,11 +1,8 @@
-#ifndef DataFormats_SiPixelClusterSoA_Alpaka_Classes_CUDA_h
-#define DataFormats_SiPixelClusterSoA_Alpaka_Classes_CUDA_h
+#ifndef DataFormats_SiPixelClusterSoA_src_alpaka_classes_cuda_h
+#define DataFormats_SiPixelClusterSoA_src_alpaka_classes_cuda_h
 
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/Common/interface/DeviceProduct.h"
-#include "DataFormats/SiPixelClusterSoA/interface/SiPixelClustersSoA.h"
 #include "DataFormats/SiPixelClusterSoA/interface/alpaka/SiPixelClustersCollection.h"
-#include "DataFormats/SiPixelClusterSoA/interface/SiPixelClustersDevice.h"
-#include "DataFormats/SiPixelClusterSoA/interface/SiPixelClustersHost.h"
 
-#endif  // DataFormats_SiPixelClusterSoA_Alpaka_Classes_CUDA_h
+#endif  // DataFormats_SiPixelClusterSoA_src_alpaka_classes_cuda_h
--- DataFormats/SiPixelClusterSoA/src/alpaka/classes_rocm.h
+++ DataFormats/SiPixelClusterSoA/src/alpaka/classes_rocm.h
@@ -1,10 +1,8 @@
-#ifndef DataFormats_SiPixelClusterSoA_Alpaka_Classes_rocm_h
-#define DataFormats_SiPixelClusterSoA_Alpaka_Classes_rocm_h
+#ifndef DataFormats_SiPixelClusterSoA_src_alpaka_classes_rocm_h
+#define DataFormats_SiPixelClusterSoA_src_alpaka_classes_rocm_h
 
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/Common/interface/DeviceProduct.h"
-#include "DataFormats/SiPixelClusterSoA/interface/SiPixelClustersSoA.h"
 #include "DataFormats/SiPixelClusterSoA/interface/alpaka/SiPixelClustersCollection.h"
-#include "DataFormats/SiPixelClusterSoA/interface/SiPixelClustersDevice.h"
 
-#endif  // DataFormats_SiPixelClusterSoA_Alpaka_Classes_rocm_h
+#endif  // DataFormats_SiPixelClusterSoA_src_alpaka_classes_rocm_h
