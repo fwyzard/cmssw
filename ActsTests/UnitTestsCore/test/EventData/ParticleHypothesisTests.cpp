feb40e8d13b18576b1450bde5fb58e5c46cbbe66
Andrea Bocci <andrea.bocci@cern.ch>
2025-10-18 11:09:49 +0200

    Reorganise the Acts tests to build inside CMSSW
    CMakeLists.txt files are translated into BuildFile.xml files and removed.
    
    These tests are reorganised as follows:
      - Tests/CommonHelpers/            as ActsTests/CommonHelpers/
      - Tests/Data/                     as ActsTests/CommonHelpers/data/
      - Tests/IntegrationTests/         as ActsTests/IntegrationTests/
      - Tests/UnitTests/Benchmarks/     as ActsTests/UnitTestsBenchmarks/
      - Tests/UnitTests/Core/           as ActsTests/UnitTestsCore/
      - Tests/UnitTests/Plugins/ActSVG/ as ActsTests/UnitTestsActSVG/
      - Tests/UnitTests/Plugins/Covfie/ as ActsTests/UnitTestsCovfie/
      - Tests/UnitTests/Plugins/DD4hep/ as ActsTests/UnitTestsDD4hep/
      - Tests/UnitTests/Plugins/Detray/ as ActsTests/UnitTestsDetray/
      - Tests/UnitTests/Plugins/Geant4/ as ActsTests/UnitTestsGeant4/
      - Tests/UnitTests/Plugins/Json/   as ActsTests/UnitTestsJson/
      - Tests/UnitTests/Plugins/Root/   as ActsTests/UnitTestsRoot/
    
    The other tests are removed:
      - Tests/IntegrationTests/Fatras/
      - Tests/UnitTests/Alignment/
      - Tests/UnitTests/Examples/
      - Tests/UnitTests/Fatras/
      - Tests/UnitTests/Plugins/EDM4hep/
      - Tests/UnitTests/Plugins/FastJet/
      - Tests/UnitTests/Plugins/FpeMonitoring/
      - Tests/UnitTests/Plugins/GeoModel/
      - Tests/UnitTests/Plugins/Gnn/
      - Tests/UnitTests/Plugins/Hashing/
      - Tests/UnitTests/Plugins/Podio/
    
    The followting tests are currently failing:
      - ActsUnitTestAtlasStepper
      - ActsUnitTestCorrectedTransformFreeToBound
      - ActsUnitTestCylinderVolumeStack
      - ActsUnitTestDD4hepDetectorElement
      - ActsUnitTestSeedFinder

---
+++ ActsTests/UnitTestsCore/test/EventData/ParticleHypothesisTests.cpp
@@ -0,0 +1,116 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 CERN for the benefit of the ACTS project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include <boost/test/unit_test.hpp>
+
+#include "Acts/Definitions/Units.hpp"
+#include "Acts/EventData/Charge.hpp"
+#include "Acts/EventData/ParticleHypothesis.hpp"
+#include "ActsTests/CommonHelpers/interface/FloatComparisons.hpp"
+
+#include <limits>
+#include <type_traits>
+
+using namespace Acts;
+using namespace Acts::UnitLiterals;
+
+static auto eps = std::numeric_limits<double>::epsilon();
+
+namespace ActsTests {
+
+BOOST_AUTO_TEST_SUITE(EventDataSuite)
+
+BOOST_AUTO_TEST_CASE(Neutral) {
+  auto p = NeutralParticleHypothesis::pion0();
+
+  BOOST_CHECK_EQUAL(p.extractCharge(1.23), 0_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(2.54), 0_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-1.98), 0_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-2.23), 0_e);
+  CHECK_CLOSE_REL(p.extractMomentum(1 / 64_GeV), 64_GeV, eps);
+  CHECK_CLOSE_REL(p.extractMomentum(1 / 128_MeV), 128_MeV, eps);
+}
+
+BOOST_AUTO_TEST_CASE(SinglyCharged) {
+  auto p = SinglyChargedParticleHypothesis::pion();
+
+  BOOST_CHECK_EQUAL(p.extractCharge(1.23), 1_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(2.54), 1_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-1.98), -1_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-2.23), -1_e);
+  CHECK_CLOSE_REL(p.extractMomentum(1_e / 64_GeV), 64_GeV, eps);
+  CHECK_CLOSE_REL(p.extractMomentum(1_e / 128_MeV), 128_MeV, eps);
+  CHECK_CLOSE_REL(p.extractMomentum(-1_e / 128_MeV), 128_MeV, eps);
+}
+
+BOOST_AUTO_TEST_CASE(NonNeutralChargeSingle) {
+  auto p = NonNeutralChargedParticleHypothesis::pion();
+
+  BOOST_CHECK_EQUAL(p.extractCharge(1.23), 1_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(2.54), 1_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-1.98), -1_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-2.23), -1_e);
+  CHECK_CLOSE_REL(p.extractMomentum(1_e / 64_GeV), 64_GeV, eps);
+  CHECK_CLOSE_REL(p.extractMomentum(1_e / 128_MeV), 128_MeV, eps);
+  CHECK_CLOSE_REL(p.extractMomentum(-1_e / 128_MeV), 128_MeV, eps);
+}
+
+BOOST_AUTO_TEST_CASE(NonNeutralChargeMultiple) {
+  auto p = NonNeutralChargedParticleHypothesis::pionLike(3_e);
+
+  BOOST_CHECK_EQUAL(p.extractCharge(1.23), 3_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(2.54), 3_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-1.98), -3_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-2.23), -3_e);
+  CHECK_CLOSE_REL(p.extractMomentum(3_e / 64_GeV), 64_GeV, eps);
+  CHECK_CLOSE_REL(p.extractMomentum(3_e / 128_MeV), 128_MeV, eps);
+  CHECK_CLOSE_REL(p.extractMomentum(-3_e / 128_MeV), 128_MeV, eps);
+}
+
+BOOST_AUTO_TEST_CASE(AnyChargeNeutral) {
+  auto p = ParticleHypothesis::pion0();
+
+  BOOST_CHECK_EQUAL(p.extractCharge(1.23), 0_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(2.54), 0_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-1.98), 0_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-2.23), 0_e);
+  CHECK_CLOSE_REL(p.extractMomentum(1 / 64_GeV), 64_GeV, eps);
+  CHECK_CLOSE_REL(p.extractMomentum(1 / 128_MeV), 128_MeV, eps);
+
+  // negative inputs should not occur for neutral particles
+  // the result is not defined, but we check it anyway
+  CHECK_CLOSE_REL(p.extractMomentum(-1 / 128_MeV), -128_MeV, eps);
+}
+
+BOOST_AUTO_TEST_CASE(AnyChargeSingle) {
+  auto p = ParticleHypothesis::pion();
+
+  BOOST_CHECK_EQUAL(p.extractCharge(1.23), 1_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(2.54), 1_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-1.98), -1_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-2.23), -1_e);
+  CHECK_CLOSE_REL(p.extractMomentum(1_e / 64_GeV), 64_GeV, eps);
+  CHECK_CLOSE_REL(p.extractMomentum(1_e / 128_MeV), 128_MeV, eps);
+  CHECK_CLOSE_REL(p.extractMomentum(-1_e / 128_MeV), 128_MeV, eps);
+}
+
+BOOST_AUTO_TEST_CASE(AnyChargeMultiple) {
+  auto p = ParticleHypothesis::pionLike(3_e);
+
+  BOOST_CHECK_EQUAL(p.extractCharge(1.23), 3_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(2.54), 3_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-1.98), -3_e);
+  BOOST_CHECK_EQUAL(p.extractCharge(-2.23), -3_e);
+  CHECK_CLOSE_REL(p.extractMomentum(3_e / 64_GeV), 64_GeV, eps);
+  CHECK_CLOSE_REL(p.extractMomentum(3_e / 128_MeV), 128_MeV, eps);
+  CHECK_CLOSE_REL(p.extractMomentum(-3_e / 128_MeV), 128_MeV, eps);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}  // namespace ActsTests
---
+++ ActsTests/UnitTestsCore/test/Geometry/TrackingGeometryCreationTests.cpp
@@ -0,0 +1,43 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 CERN for the benefit of the ACTS project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include <boost/test/unit_test.hpp>
+
+#include "Acts/Geometry/GeometryContext.hpp"
+#include "ActsTests/CommonHelpers/interface/CubicTrackingGeometry.hpp"
+#include "ActsTests/CommonHelpers/interface/CylindricalTrackingGeometry.hpp"
+
+using namespace Acts;
+
+namespace ActsTests {
+
+// Create a test context
+GeometryContext tgContext = GeometryContext();
+
+BOOST_AUTO_TEST_SUITE(GeometrySuite)
+
+BOOST_AUTO_TEST_CASE(CylindricalTrackingGeometryTest) {
+  CylindricalTrackingGeometry cGeometry(tgContext);
+  auto tGeometry = cGeometry();
+  BOOST_CHECK_NE(tGeometry, nullptr);
+
+  BOOST_CHECK(tGeometry->geometryVersion() ==
+              TrackingGeometry::GeometryVersion::Gen1);
+}
+
+BOOST_AUTO_TEST_CASE(CubicTrackingGeometryTest) {
+  CubicTrackingGeometry cGeometry(tgContext);
+  auto tGeometry = cGeometry();
+  BOOST_CHECK_NE(tGeometry, nullptr);
+  BOOST_CHECK(tGeometry->geometryVersion() ==
+              TrackingGeometry::GeometryVersion::Gen1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}  // namespace ActsTests
